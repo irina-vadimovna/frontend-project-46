47b3edf2b03b7150157340b2d0d434f09ec5689e
Irina Vadimovna <evdokimovaira2000@mail.ru>
2024-05-19 19:57:48 +0700

    added secret

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('json gendiff', () => {
